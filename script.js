7e2eec9042f9d52a288dc1ae1df84d99696e4e41
Ankit Kumar <roy.ankit5008@gmail.com>
2023-01-24 23:44:19 +0530

    new commit

--- script.js
+++ script.js
@@ -29,6 +29,24 @@ searchByIdBtn.onclick = () =>{
     searchHero('',id);
 }
 
+
+const createStats = (str,obj) =>{
+    const stats = document.getElementById('stats');
+    str += `name : ${obj.name}<br>`;
+    str += `id : ${obj.id}<br>`;
+    const biography = Object.values(obj.biography);
+    str += `full-name : ${biography[0]}<br>`
+    str += `place-of-birth : ${biography[3]}<br>`
+    str += `first-appearance : ${biography[4]}<br>`
+    str += `publisher : ${biography[5]}<br>`
+    str += `alignment : ${biography[6]}<br>`
+    const powerstats = Object.keys(obj.powerstats);
+    powerstats.forEach(item => {
+        str += `${item} : ${obj.powerstats[item]}<br>`;
+    });
+    stats.innerHTML = str;
+}
+
 //main function to search hero
 const searchHero = (name,id) =>{
     let p;
@@ -43,6 +61,15 @@ const searchHero = (name,id) =>{
     }).then(json => {
         if(id==0){
             heroImage.src = json.results[0].image.url;
+            createStats('',json.results[0]);
+            // const powerstats = Object.keys(json.results[0].powerstats);
+            // console.log(powerstats);
+
+            // powerstats.forEach(item => {
+            //     statsString += `${item} ${json.results[0].powerstats[item]}<br>`;
+            // });
+            // console.log(statsString);
+            // stats.innerHTML = statsString;
         }
         else{
             heroImage.src = json.image.url;
